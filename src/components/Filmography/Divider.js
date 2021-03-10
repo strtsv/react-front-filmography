43e47ddb1f1b287d8bc6a8a369bdc2e3630e2df3
jsusilo <enr@bk.ru>
2021-03-10 19:44:38 +0500

    Filmography mostpopular comp

---
+++ src/components/Filmography/Divider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Divider extends React.Component {
+  render() {
+    return <div className="decorative-divider" />;
+  }
+}
+
+export default Divider;
---
+++ src/components/Filmography/MostPopular.js
@@ -0,0 +1,112 @@
+import React from "react";
+
+class MostPopular extends React.Component {
+  render() {
+    return (
+      <section className="section section-md bg-pattern-1">
+        <div className="container">
+          <div className="row row-50">
+            <div className="col-lg-8">
+              <h3 className="text-center title-text mod-2"> Most popular</h3>
+              <ul className="list-index">
+                <li>
+                  <div className="unit unit-sm d-block d-sm-flex text-center text-sm-left">
+                    <div className="unit-left">
+                      <div className="list-index-count" />
+                    </div>
+                    <div className="unit-body">
+                      <h4 className="text-gray-790">
+                        <a className="link-1" href="#">
+                          Magister velums, tanquam teres fides.
+                        </a>
+                      </h4>
+                      <p>
+                        Nunquam fallere adelphis. Ubi est barbatus luna?
+                        Particulas sunt indexs de germanus mineralis. Gratis
+                        exemplars ducunt ad gemna.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="unit unit-sm d-block d-sm-flex text-center text-sm-left">
+                    <div className="unit-left">
+                      <div className="list-index-count" />
+                    </div>
+                    <div className="unit-body">
+                      <h4 className="text-gray-790">
+                        <a className="link-1" href="#">
+                          Magister velums, tanquam teres fides.
+                        </a>
+                      </h4>
+                      <p>
+                        Nunquam fallere adelphis. Ubi est barbatus luna?
+                        Particulas sunt indexs de germanus mineralis. Gratis
+                        exemplars ducunt ad gemna.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="unit unit-sm d-block d-sm-flex text-center text-sm-left">
+                    <div className="unit-left">
+                      <div className="list-index-count" />
+                    </div>
+                    <div className="unit-body">
+                      <h4 className="text-gray-790">
+                        <a className="link-1" href="#">
+                          Magister velums, tanquam teres fides.
+                        </a>
+                      </h4>
+                      <p>
+                        Nunquam fallere adelphis. Ubi est barbatus luna?
+                        Particulas sunt indexs de germanus mineralis. Gratis
+                        exemplars ducunt ad gemna.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-4 text-center text-lg-left">
+              <h3 className="title-text mod-3 text-center"> Filmography</h3>
+              <h4 className="text-gray-790">
+                <a className="link-1" href="#">
+                  Purus vel cursus commod
+                </a>
+              </h4>
+              <p>
+                Nulla tristique ligula ut velit pharetra condimentum, bitur
+                magna turpis, interdum a nibh sit amet, ornare luc.
+              </p>
+              <div className="link-list-wrapper">
+                <ul className="link-list">
+                  <li>
+                    <a href="#">Pellentesque ultricies felis</a>
+                  </li>
+                  <li>
+                    <a href="#">Purus, id eleifend dolor consequat ac.</a>
+                  </li>
+                  <li>
+                    <a href="#">Etiam cursus augue eget sem</a>
+                  </li>
+                  <li>
+                    <a href="#">Condimentum, non volutpat lacus</a>
+                  </li>
+                  <li>
+                    <a href="#">Fermentum. Fusce facilisis mi eget</a>
+                  </li>
+                  <li>
+                    <a href="#">Posuere interdum. Etiam</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MostPopular;
