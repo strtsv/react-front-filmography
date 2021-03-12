2bcdf33470981f77d0667a36e420138f64b649b2
jsusilo <enr@bk.ru>
2021-03-13 00:28:17 +0500

    gallery

---
+++ src/components/Gallery/MyGallery.js
@@ -0,0 +1,253 @@
+import React from "react";
+
+class MyGallery extends React.Component {
+  render() {
+    return (
+      <section className="section section-md bg-pattern-1">
+        <div className="container">
+          <div className="text-center">
+            <h3 className="title-text">my gallery</h3>
+          </div>
+          <div className="row row-40 justify-content-center">
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-01-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-01-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Pellentesque
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Vivamus porta orci sapien, eu volutpat neque tincide socisera
+                  natoqu.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-02-750x500-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-02-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Etiam interdum ipsum
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Etiam auctor sem tortor, nec fermentum mauris sollicitudin
+                  non. Donec gravida.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-03-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-03-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Nulla nec magna enim
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Donec gravida massa neque, a auctor lect us elementum non.
+                  Vivamus porta orci.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-04-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-04-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Et ultricies in, tempus
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Cumaser kertyase socisera natoqu eagnis dulmuese feugiata
+                  lecenaseras destricies.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-05-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-05-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Quisque euismod dolor
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Gravida massa neque, a auctor lectus elem entum non. Vivamus
+                  porta orci.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-06-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-06-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Sed diam nunc, finibus
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Nec fermentum mauris sollicitudin non, donec gravida massa
+                  neque, a auctor.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-07-1200x800-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-07-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Cras vel laoreet augue
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Vivamus porta orci sapien, eu volutpat neque tincidunt ac.
+                  Maecenas posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-08-750x720-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-08-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Maximus convallis
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Citudin non. Donec gravida massa neque, vivamus porta orci
+                  sapien.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              <div className="box-zita">
+                <a
+                  className="thumb"
+                  data-lightgallery="item"
+                  href="images/box-zita-img-09-750x1125-original.jpg"
+                >
+                  <img
+                    src="images/box-zita-img-09-300x265.jpg"
+                    alt
+                    width={300}
+                    height={265}
+                  />
+                  <span className="thumb-overlay" />
+                </a>
+                <h4 className="text-gray-790">
+                  <a className="link-1" href="#">
+                    Sed rhoncus cursus ult
+                  </a>
+                </h4>
+                <p className="lh-1 text-gray-500">
+                  Cumaser kertyase socisera natoqu eagnis dulmuese feugiata
+                  lecenaseras destricies.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MyGallery;
