add0100d04a687a8f4d6633f98bcb1cca9c89fe9
jsusilo <enr@bk.ru>
2021-03-14 00:19:26 +0500

    gallery UsefulInfo cmp

---
+++ src/components/Gallery/UsefulInfo.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class UsefulInfo extends React.Component {
+  render() {
+    return (
+      <section className="section section-md context-dark bg-gray-800">
+        <div className="container">
+          <div className="text-center">
+            <h3 className="title-text">Useful info</h3>
+            <h6>
+              Etiam interdum ipsum id finibus faucibus. Nulla nec magna mollis
+            </h6>
+            <p className="text-gray-300">
+              Anritma hasesera deyulety. Cumaser kertyase socisera natoqu eagnis
+              dist mte dulmuese feugiata lecenaser mutrease stricies ser
+              datyfena nec sit amm easer erment. Utdolores urliquam
+              eratesasvpatis. Miastas kertyase fertasbertas kertyasera miasears
+              letasener dertasera niytrasasa derodes Integer rutrum ante eu
+              lacus uisque nulla.
+            </p>
+            <a className="button button-radius button-primary" href="#">
+              more
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default UsefulInfo;
